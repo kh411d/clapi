8b7ecccf37b2434e60ce4d423ba140d661c8bb1e
Khalid Adisendjaja <kh411dz@gmail.com>
2020-09-16 06:36:02 +0700

    adding function handler

---
+++ api/fn.go
@@ -0,0 +1,15 @@
+package api
+
+func getClaps() map[string]interface{} {
+	return map[string]interface{}{
+		"StatusCode": 200,
+		"Body":       "Hello, World",
+	}
+}
+
+func addClap() map[string]interface{} {
+	return map[string]interface{}{
+		"StatusCode": 200,
+		"Body":       "Hello, World",
+	}
+}
