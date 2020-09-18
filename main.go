f96056f677c6e1b03018c222faa5e0b8d2b59065
Khalid Adisendjaja <kh411dz@gmail.com>
2020-09-18 08:18:58 +0700

    swith to os to detect env var

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/kh411d/clapi/api"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	v := viper.New()
-	v.AutomaticEnv()
 
-	if v.Get("AWS_LAMBDA_FUNCTION_NAME") == nil {
-		http.HandleFunc("/clap", api.ServeHTTP)
+	if os.Getenv("AWS_LAMBDA_FUNCTION_NAME") == "" {
+		http.HandleFunc("/", api.ServeHTTP)
 		http.ListenAndServe(":3000", nil)
 	} else {
 		// Make the handler available for Remote Procedure Call by AWS Lambda
